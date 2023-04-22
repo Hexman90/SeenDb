aaa118ea23fabdc39adfb6e36c2c733d22e82554
Hexman90 <120564747+Hexman90@users.noreply.github.com>
2023-04-22 09:20:41 +0100

    Update app.py
    added send_from_directory module from flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, send_from_directory
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
